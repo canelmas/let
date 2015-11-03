923982fcb8500678dd5355e24d4802dcd8238ca6
Can Elmas <Can.Elmas@monitise.com>
2015-11-03 17:48:10 +0300

    set retention policy to Runtime

--- let-annotations/src/main/java/com/canelmas/let/AskPermission.java
+++ let-annotations/src/main/java/com/canelmas/let/AskPermission.java
@@ -23,7 +23,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Documented
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface AskPermission {
 
